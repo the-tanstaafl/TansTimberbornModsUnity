f83224daf21cd886b4860847c42d13ccc4c2e5e9
KnatteAnka <simon_wiklund@hotmail.com>
2022-08-27 22:16:34 +0200

    WaterExtention new mod for Water stuff Importing Irrigationtower V2

---
+++ Assets/WaterExtention/Plugin.cs
@@ -0,0 +1,34 @@
+using System;
+using BepInEx;
+using BepInEx.Logging;
+using TimberbornAPI;
+using TimberbornAPI.Common;
+
+//using TimberbornAPI.AssetLoaderSystem.ResourceAssetPatch;
+//using UnityEngine;
+using HarmonyLib;
+
+namespace IrrigationTowerV2
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    [BepInDependency("com.timberapi.timberapi")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PluginGuid = "knatte.irrigationtowerv2";
+        public const string PluginName = "IrrigationTowerV2";
+        public const string PluginVersion = "1.0.0";
+
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = Logger;
+            
+            Log.LogInfo($"Loaded {PluginName} Version: {PluginVersion}!");
+
+            TimberAPI.AssetRegistry.AddSceneAssets(PluginGuid, SceneEntryPoint.Global);
+            //new Harmony(PluginGuid).PatchAll();
+        }
+    }
+    
+}
