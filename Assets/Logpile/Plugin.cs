1d23369f33509610afb251178985bb5d300e142d
KnatteAnka <simon_wiklund@hotmail.com>
2022-09-19 22:32:58 +0200

    Logpile update

--- Assets/Logpile/Plugin.cs
+++ Assets/Logpile/Plugin.cs
@@ -1,16 +1,23 @@
 using System;
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
+using Bindito.Core;
 //using TimberbornAPI;
 //using TimberbornAPI.Common;
+using TimberApi.ConsoleSystem;
+using TimberApi.DependencyContainerSystem;
+using TimberApi.ModSystem;
 using Timberborn.Warehouses;
 
 using HarmonyLib;
 
 namespace TinyLogpile
 {
-    //[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
-    //[BepInDependency("com.timberapi.timberapi")]
+    //-------------------- Timber API 0.4.4 ------------------------------
+    /*[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    [BepInDependency("com.timberapi.timberapi")]
+
     public class Plugin : BaseUnityPlugin
     {
         public const string PluginGuid = "knattetobbert.tinylogpile2";
@@ -28,7 +35,24 @@ namespace TinyLogpile
             //TimberAPI.AssetRegistry.AddSceneAssets(PluginGuid, SceneEntryPoint.Global);
             new Harmony(PluginGuid).PatchAll();
         }
+    }*/
+    //-------------------- Timber API 0.5.0 ------------------------------
+    public class Plugin : IModEntrypoint
+    {
+        public const string PluginGuid = "knattetobbert.tinylogpile2";
+        public const string PluginName = "Tinylogpile";
+        public const string PluginVersion = "1.0.7";
+        
+        public void Entry(IMod mod, IConsoleWriter consoleWriter)
+        {
+            //Log = Logger;
+            
+            //Log.LogInfo($"Loaded {PluginName} Version: {PluginVersion}!");
+            new Harmony(PluginGuid).PatchAll();
+        }
     }
+    
+
     
     [HarmonyPatch(typeof(LogPileStockpileVisualizer), "InitializeLogStack", new Type[] { typeof(int) , typeof(int) })]
     public class FixInitialStackPatch
